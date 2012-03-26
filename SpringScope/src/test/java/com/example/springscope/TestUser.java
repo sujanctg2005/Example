b5b40bf0b3adc14daea3e73bea8a468cb8e0bc05
Sujan Kishor Nath <sujanctg2005@gmail.com>
2012-03-26 02:49:39 +0200

    test

---
+++ SpringScope/src/test/java/com/example/springscope/TestUser.java
@@ -0,0 +1,11 @@
+package com.example.springscope;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({})
+public class TestUser {
+
+}
